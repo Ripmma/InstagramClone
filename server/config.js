0c328431598e7d2cfb69d94754e00842bc9dddca
Ripmma <Ripmma@gmail.com>
2017-06-01 15:45:10 -0400

    Part 11, 12

--- server/config.js
+++ server/config.js
@@ -3,3 +3,9 @@ module.exports = {
 	clientSecret: process.env.clientSecret || ' 9642b6417f894666a920d6d19a576344',
 	tokenSecret: process.env.tokenSecret || 'Aloha'
 }
+
+module.exports = {
+  db: process.env.db || 'localhost',
+  tokenSecret: process.env.tokenSecret || 'pick a hard to guess string'
+
+};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-var bcrypt = require('bcryptjs');
+var bcrypt = require('bcrypt-nodejs');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var express = require('express');
@@ -8,6 +8,19 @@ var mongoose = require('mongoose');
 var path = require('path');
 var request = require('request');
 
+var config = require('./config');
+var User = mongoose.model('User', new mongoose.Schema({
+  instagramId: { type: String, index: true },
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, select: false },
+  username: String,
+  fullName: String,
+  picture: String,
+  accessToken: String
+}));
+
+mongoose.connect(config.db);
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -16,6 +29,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+function createToken(user) {
+  var payload = {
+    exp: moment().add(14, 'days').unix(),
+    iat: moment().unix(),
+    sub: user._id
+  };
+
+  return jwt.encode(payload, config.tokenSecret);
+}
+
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
